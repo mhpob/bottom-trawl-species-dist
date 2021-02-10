9c94ff2664eeaa56c4e4052d2f86a84d7a1e2fa5
Mike O'Brien <mikeob9@gmail.com>
2021-02-10 11:08:38 -0500

    Add species names to survdat.

--- Code/add_data_to_survdat.R
+++ Code/add_data_to_survdat.R
@@ -1,4 +1,46 @@
+library(data.table)
+
+# Load and manipulate NOAA EDAB's compiled survey data ----
+load('data/noaa_edab_survdat.rdata')
+
+setnames(survdat, tolower)
+
+
+# Add species names ----
+species_list <- fread(
+  input = 'data/svdbs_supporttables/svdbs_svspecies_list.csv',
+  col.names = tolower,
+  fill = T
+)
+
+## Quick data cleaning
+species_list[!grepl('^\\d', svspp), ':='(
+  comname = paste(comname, svspp, v1),
+  svspp = v2)]
+species_list[, c('v1', 'v2') := NULL]
+species_list[, svspp := as.numeric(svspp)]
+species_list[, c('sciname', 'comname') :=
+               lapply(.SD, tolower), .SDcols = c('sciname', 'comname')]
+
+## Join species names
+### Note that 726 observations don't have an associated species name
+survdat <- species_list[survdat, on = 'svspp']
+
+
+
+
+# Add sediment data ----
+## Unique info by trawl
+key <- unique(survdat, by = c('cruise6', 'station', 'stratum', 'tow'))
+
+## Pick needed columns
+key <- key[, .(cruise6, station, stratum, tow, season, lat, lon)]
+
+
+## Load bottom sediment type
 library(sf)
+
+## Other shapefiles -- not used at the moment
 # bot_mab <- st_read('data/mapping/benthic_habitats/benhab_mab.shp',
 #                    query = 'SELECT
 #                               SEDIMENT as sediment,
@@ -19,38 +61,35 @@ library(sf)
 bottom_type <- st_read('data/mapping/tncbenthic/benthic.gdb',
                        layer = 'benthic_sediment')
 
-library(data.table)
+key <- st_as_sf(key,
+                coords = c('lon', 'lat'),
+                crs = 4326)
+key <- st_transform(key,
+                    st_crs(bottom_type))
 
-load('data/noaa_edab_survdat.rdata')
 
+
+key <- st_join(key, bottom_type, join = st_intersects)
+key <- st_transform(key, 4326)
+key$lat <- st_coordinates(key)[, 2]
+key$lon <- st_coordinates(key)[, 1]
+
+key <- data.table(key)
+key <- key[, !c('ID', 'GRIDCODE', 'GLOBALID', 'SHAPE_Length', 'SHAPE_Area',
+                'geometry', 'lat', 'lon')]
+
+
+
+survdat <- survdat[key, on = c('cruise6', 'station', 'stratum', 'tow', 'season')]
+
+
+
+# Get ready to export ----
 setnames(survdat, tolower)
 
-# Drop length
-survdat[, c('length', 'numlen') := NULL]
+survdat <- survdat[, .(cruise6, station, stratum, tow, svvessel, year, season, lat, lon,
+                       est_towdate, depth, sediment, grpsed, surftemp, surfsalin, bottemp,
+                       botsalin, sciname, comname, svspp, catchsex, abundance, biomass,
+                       length, numlen)]
 
-# Unique info by trawl
-survdat <- unique(survdat, by = c('cruise6', 'station', 'stratum', 'tow'))
-
-
-sp_survdat <- st_as_sf(survdat,
-                       coords = c('lon', 'lat'),
-                       crs = 4326)
-sp_survdat <- st_transform(sp_survdat,
-                           st_crs(bottom_type))
-
-
-
-sp_survdat <- st_join(sp_survdat, bottom_type, join = st_intersects)
-sp_survdat <- st_transform(sp_survdat, 4326)
-sp_survdat$lat <- st_coordinates(sp_survdat)[, 2]
-sp_survdat$lon <- st_coordinates(sp_survdat)[, 1]
-sp_survdat <- data.table(sp_survdat)
-sp_survdat <- sp_survdat[, c(1:4, 20:21)]
-load('data/noaa_edab_survdat.rdata')
-
-setnames(survdat, tolower)
-
-
-sp_survdat <- survdat[sp_survdat, on = c('cruise6', 'station', 'stratum', 'tow')]
-
-fwrite(sp_survdat, 'data derived/survdat_w_sediment.gz')
+fwrite(survdat, 'data derived/survdat_names_sed.gz')
